400890d6557064842d9b51ed359df945c5350979
Pamela Murillo <pamelamurilloa@gmail.com>
2023-07-26 23:20:56 -0600

    Se arregló el footer, se añadió la imagen al about us, se añadió el rating

--- js/load_post.js
+++ js/load_post.js
@@ -15,25 +15,31 @@ for (let category of categories) {
 console.log(postInformation.title);
 console.log(postInformation.imgSource);
 
-const postSpace = document.getElementsByClassName("main-content")[0];
+const postSpace = document.getElementsByClassName('main-content')[0];
 
-const heroComponent = document.getElementsByClassName("hero-component")[0];
-const postTitle = document.getElementsByClassName("tile-hero-component")[0];
+const heroComponent = document.getElementsByClassName('hero-component')[0];
+const postTitle = document.getElementsByClassName('tile-hero-component')[0];
 
-heroComponent.style.backgroundImage = "url("+postInformation.imgSource+")";
+heroComponent.style.backgroundImage = 'url('+postInformation.imgSource+')';
 postTitle.innerHTML = postInformation.title;
 
-let postContent = document.createElement("div");
+let postContent = document.createElement('div');
 postContent.innerHTML = `
             <div class="post-subinformation">
-                <h4>${postInformation.author}</h4>
-                <h4>${postInformation.date}</h4>
-                <h4>${postInformation.rating}</h4>
+                <h4>Autor: ${postInformation.author}</h4>
+                <h4>Fecha: ${postInformation.date}</h4>
+                <div class="rating-distribution">
+                    <img class="rating" src="../images/rating.png"></img>
+                    <img class="rating" src="../images/rating.png"></img>
+                    <img class="rating" src="../images/rating.png"></img>
+                    <img class="rating" src="../images/rating.png"></img>
+                    <img class="rating" src="../images/rating.png"></img>
+                </div>
             </div>
             <div class="post-content-zone">
                 ${postInformation.completeInformation}
             </div>
         `;
 
-postSpace.className = "post-content";
+postSpace.className = 'post-conten';
 postSpace.appendChild(postContent);
--- js/posts.js
+++ js/posts.js
@@ -5,7 +5,7 @@ var geologicalPosts = [
         title:"Cámbrico", 
         author:"PM",
         date:"24/10/2021",
-        rating:5,
+        rating:1,
         summary:"El período Cámbrico fue testigo de una explosión de diversidad biológica conocida como la 'explosión cámbrica'. Aparecieron los primeros animales con conchas duras, esqueletos y estructuras complejas. Se formaron los primeros artrópodos, moluscos, equinodermos y trilobites. Los océanos estaban dominados por criaturas marinas y no había vida en la tierra firme.",
         completeInformation:"El período Cámbrico fue testigo de una explosión de diversidad biológica conocida como la 'explosión cámbrica'. Aparecieron los primeros animales con conchas duras, esqueletos y estructuras complejas. Se formaron los primeros artrópodos, moluscos, equinodermos y trilobites. Los océanos estaban dominados por criaturas marinas y no había vida en la tierra firme."
     },
@@ -27,7 +27,7 @@ var geologicalPosts = [
         title:"Silúrico", 
         author:"PM",
         date:"01/07/2021",
-        rating:5,
+        rating:3,
         summary:"Durante el período Silúrico, los océanos se volvieron más oxigenados y se formaron extensos bosques de algas y plantas terrestres primitivas. Los peces desarrollaron aletas más fuertes y pudieron salir del agua brevemente. También aparecieron los primeros insectos y arácnidos terrestres.",
         completeInformation:"Durante el período Silúrico, los océanos se volvieron más oxigenados y se formaron extensos bosques de algas y plantas terrestres primitivas. Los peces desarrollaron aletas más fuertes y pudieron salir del agua brevemente. También aparecieron los primeros insectos y arácnidos terrestres."    },
 
@@ -37,7 +37,7 @@ var geologicalPosts = [
         title:"Devónico", 
         author:"PM",
         date:"18/09/2020",
-        rating:5,
+        rating:2,
         summary:"En el período Devónico, los peces con mandíbulas se diversificaron y se convirtieron en los principales depredadores en los océanos. Surgieron los primeros bosques verdaderos con árboles y plantas terrestres más avanzadas. Los insectos y arácnidos se volvieron más diversos y adaptados a la vida en tierra firme.",
         completeInformation:"En el período Devónico, los peces con mandíbulas se diversificaron y se convirtieron en los principales depredadores en los océanos. Surgieron los primeros bosques verdaderos con árboles y plantas terrestres más avanzadas. Los insectos y arácnidos se volvieron más diversos y adaptados a la vida en tierra firme."
     },
@@ -59,7 +59,7 @@ var geologicalPosts = [
         title:"Pérmico", 
         author:"PM",
         date:"14/3/2021",
-        rating:5,
+        rating:3,
         summary:"El período Pérmico presenció la formación de Pangea, un supercontinente que unió la mayoría de las masas terrestres. Los reptiles se convirtieron en los animales dominantes y aparecieron los primeros mamíferos y los primeros dinosaurios. Sin embargo, al final del Pérmico, ocurrió la extinción masiva más grande de la historia de la Tierra, eliminando alrededor del 96% de las especies marinas y el 70% de las especies terrestres.",
         completeInformation:"El período Pérmico presenció la formación de Pangea, un supercontinente que unió la mayoría de las masas terrestres. Los reptiles se convirtieron en los animales dominantes y aparecieron los primeros mamíferos y los primeros dinosaurios. Sin embargo, al final del Pérmico, ocurrió la extinción masiva más grande de la historia de la Tierra, eliminando alrededor del 96% de las especies marinas y el 70% de las especies terrestres."
     }
@@ -69,22 +69,24 @@ var geologicalPosts = [
 var otherPosts = [
     {
         id:7,
-        imgSource:"../images/cambrian.jpeg",
-        title:"Titulo", 
+        imgSource:"../images/mamut.jpeg",
+        title:"Mamut lanudo", 
         author:"PM",
-        date:"10/7/2020",
-        rating:5,
-        summary:"Durante el período Carbonífero, los bosques se expandieron enormemente y se formaron grandes depósitos de carbón. Los anfibios prosperaron y se diversificaron en diferentes formas y tamaños. Los insectos alcanzaron tamaños gigantes. Los primeros reptiles también aparecieron al final del período.",
+        date:"10/7/2022",
+        rating:4,
+        summary:"El mamut lanudo fue una especie de elefante que habitó durante la era del Pleistoceno, específicamente en las regiones frías de América del Norte, Europa y Asia. Se caracterizaba por su pelaje largo y grueso, que le proporcionaba una excelente adaptación al clima frío de la época. Los mamuts lanudos tenían colmillos curvados hacia arriba, que podían medir hasta 5 metros de longitud en los machos adultos, y utilizaban estos colmillos para desenterrar vegetación cubierta por la nieve y para defenderse de depredadores.",
+        completeInformation:"El mamut lanudo fue una especie de elefante que habitó durante la era del Pleistoceno, específicamente en las regiones frías de América del Norte, Europa y Asia. Se caracterizaba por su pelaje largo y grueso, que le proporcionaba una excelente adaptación al clima frío de la época. Los mamuts lanudos tenían colmillos curvados hacia arriba, que podían medir hasta 5 metros de longitud en los machos adultos, y utilizaban estos colmillos para desenterrar vegetación cubierta por la nieve y para defenderse de depredadores. Eran herbívoros, alimentándose principalmente de pasto, musgos y arbustos. Vivían en manadas familiares y migraban en busca de alimentos y condiciones más favorables. Durante el Pleistoceno, los mamuts lanudos coexistieron con los primeros humanos y fueron cazados por estos para obtener carne, piel y huesos para fabricar herramientas y refugios. Desafortunadamente, debido a la caza excesiva y los cambios climáticos, los mamuts lanudos se extinguieron alrededor de 4,000 años atrás."
     },
 
     {
         id:8,
-        imgSource:"../images/cambrian.jpeg",
-        title:"Titulo", 
+        imgSource:"../images/smilodon.jpeg",
+        title:"Smilodon", 
         author:"PM",
         date:"30/1/2020",
         rating:5,
-        summary:"Durante el período Carbonífero, los bosques se expandieron enormemente y se formaron grandes depósitos de carbón. Los anfibios prosperaron y se diversificaron en diferentes formas y tamaños. Los insectos alcanzaron tamaños gigantes. Los primeros reptiles también aparecieron al final del período.",
+        summary:"El Smilodon, también conocido como el 'tigre dientes de sable', era un felino gigante que vivió durante el Pleistoceno en América del Norte y del Sur. Aunque se le conoce popularmente como 'dientes de sable', no era un tigre, sino más cercano a los leones y tigres modernos. Su característica más distintiva era su par de colmillos largos y curvados, que podían medir hasta 30 centímetros de longitud en algunas especies.",
+        completeInformation:"El Smilodon, también conocido como el 'tigre dientes de sable', era un felino gigante que vivió durante el Pleistoceno en América del Norte y del Sur. Aunque se le conoce popularmente como 'dientes de sable', no era un tigre, sino más cercano a los leones y tigres modernos. Su característica más distintiva era su par de colmillos largos y curvados, que podían medir hasta 30 centímetros de longitud en algunas especies. El Smilodon era un depredador formidable que cazaba grandes mamíferos herbívoros, como bisontes y caballos prehistóricos. Su técnica de caza consistía en emboscar a sus presas y utilizar sus potentes mandíbulas y colmillos para dar golpes mortales. Su cráneo robusto y sus poderosos músculos en el cuello le proporcionaban una mordida excepcionalmente fuerte. A pesar de ser un depredador temible, los Smilodones se extinguieron hace aproximadamente 10,000 años, junto con muchos otros grandes mamíferos del Pleistoceno, probablemente debido a la disminución de sus presas y cambios en el entorno."
     },
 ]
 
