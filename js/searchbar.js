26f2a31743cc5c9236d1b91e38994e2afba7a7c7
Pamela Murillo <pamelamurilloa@gmail.com>
2023-08-08 16:55:48 -0600

    Terminada la zona de busqueda

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,26 +1,51 @@
 const optionsParent = document.getElementById("dropdown-search-options");
 
-
 let keyphrase = '';
 
 const updateSearchBar = () => {
     keyphrase = document.getElementById("to-search").value;
-    optionsParent.innerHTML = "";
-    let searchResults = [];
+    container.innerHTML = ""
+    // optionsParent.innerHTML = "";
+    // container.innerHTML = "";
+    // let searchResults = [];
 
     for (let category of categories) {
+        var categoryElement = document.createElement("h2");
+        categoryElement.innerHTML = category.name;
+        var postContainer = document.createElement("div");
+        container.appendChild(categoryElement);
+        
         for (let post of category.posts) 
         {
             if (post.title.toLowerCase().includes(keyphrase) || post.summary.toLowerCase().includes(keyphrase) || post.author.toLowerCase().includes(keyphrase)) {
-                searchResults.push(post);
+                // searchResults.push(post);
 
-                let optionElement = document.createElement("li");
-                optionElement.innerHTML = `<a href="#${post.id}">${post.title}</a>`;
-                optionsParent.appendChild(optionElement);
+                // let optionElement = document.createElement("li");
+                // optionElement.innerHTML = `<a href="#${post.id}">${post.title}</a>`;
+                // optionsParent.appendChild(optionElement);
+
+                var miniPost = document.createElement("div");
+                miniPost.innerHTML = `
+                    <img class="post-image" alt="post-image" src="${post.imgSource}"></img>
+                    <div class="post-title-zone">
+                        <h3><a href="/html/post_page.html?id=${post.id}">${post.title}</a></h3>
+                        <div class="post-tile-zone-subinformation">
+                            <h4>${post.author}</h4>
+                            <h4>${post.date}</h4>
+                        </div>
+                    </div>
+                    <div class="post-desc-zone">
+                        ${post.summary}
+                    </div>
+                `
+
+                miniPost.className = "post";
+
+                postContainer.appendChild(miniPost);
             }
         }
+
+        postContainer.className = "post-container";
+        container.appendChild(postContainer);
     }
-
-    console.log(searchResults);
 }
-
